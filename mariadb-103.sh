87d5a31bdc636db9dd839e71723f9d4dbd8c00ed
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2019-12-31 08:17:31 -0500

    Create mariadb-103.sh

---
+++ mariadb-103.sh
@@ -0,0 +1,20 @@
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+
+mariadb_root_reset(){
+    service mariadb stop
+    mysqld_safe --skip-grant-tables >res 2>&1 &
+    sleep 5
+    rootpassword=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+    Q1="UPDATE user SET plugin='';"
+    Q2="UPDATE user SET password=PASSWORD('$rootpassword') WHERE user='root';"
+    Q3="FLUSH PRIVILEGES;"
+    SQL="${Q1}${Q2}${Q3}"
+    mysql mysql -e "$SQL"
+    pkill mysqld
+    service mariadb restart
+    output "Your MariaDB root password is $rootpassword"
+}
+
+mariadb_root_reset
